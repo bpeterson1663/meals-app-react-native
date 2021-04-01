e17430087500799fa517dc9d17e62662846d312e
Brady Peterson <bpeterson1663@gmail.com>
2021-03-31 23:31:34 -0500

    feat(categories): render categories

--- screens/CategoriesScreen.tsx
+++ screens/CategoriesScreen.tsx
@@ -4,18 +4,35 @@ import {
     NavigationScreenProp,
     NavigationState,
   } from 'react-navigation'
-import { View, Text, StyleSheet, Button} from 'react-native'
+import { View, Text, StyleSheet, Button, FlatList, TouchableOpacity, Platform} from 'react-native'
+import { CATEGORIES } from '../data/dummy-data'
+import Category from '../models/Category'
 interface CategoriesScreenT{
+    navigationOptions?: Object,
     navigation: NavigationScreenProp<NavigationState, NavigationParams>
 }
-const CategoriesScreen: React.FC<CategoriesScreenT>  = ({navigation}): JSX.Element => {
+interface NavStatelessComponent extends React.StatelessComponent {
+    navigationOptions?: Object
+    navigation: NavigationScreenProp<NavigationState, NavigationParams>
+
+}
+
+const CategoriesScreen: React.StatelessComponent<CategoriesScreenT>  = ({navigation}): JSX.Element => {
+    const renderGridItem = (itemData: {item: Category}) => {
+        return ( 
+            <TouchableOpacity
+                style={styles.gridItem}
+                onPress={() => {
+                    navigation.navigate({routeName: 'CategoryMeals'})
+                }}>
+                <View >
+                    <Text>{itemData.item.title}</Text>
+                </View>
+            </TouchableOpacity>)
+    }
     return (
-        <View style={styles.screen}>
-            <Text>Categories Screen</Text>
-            <Button onPress={() => { 
-                navigation.navigate({routeName: 'CategoryMeals'})
-            }} title="Go To Meals Screen"></Button>
-        </View>
+        <FlatList numColumns={2} renderItem={renderGridItem} data={CATEGORIES} />
+       
     )
 }
 
@@ -24,6 +41,10 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    gridItem: {
+        flex: 1,
+        margin: 15
     }
 })
 
