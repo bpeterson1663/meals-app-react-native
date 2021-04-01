61982d9eb1694cd4e856d47ecaa3178983f624a1
Brady Peterson <bpeterson1663@gmail.com>
2021-04-01 08:21:29 -0500

    feat(defaults): set default navigation options

--- navigation/MealsNavigation.tsx
+++ navigation/MealsNavigation.tsx
@@ -3,11 +3,20 @@ import { createStackNavigator } from 'react-navigation-stack'
 import CategoriesScreen from '../screens/CategoriesScreen'
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
-
+import { Platform } from 'react-native'
+import Colors from '../constants/Colors'
 const MealsNavigator = createStackNavigator({
     Categories: CategoriesScreen,
     CategoryMeals: CategoryMealsScreen,
     MealDetail: MealDetailScreen
+},
+{
+    defaultNavigationOptions: {
+        headerStyle: {
+            backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+        },
+        headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+    }
 })
 
 export default createAppContainer(MealsNavigator);
