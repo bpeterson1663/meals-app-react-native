04c3b90ce2f1bd101e11d596b10d9853fbfc526d
Brady Peterson <bpeterson1663@gmail.com>
2021-04-06 21:02:20 -0500

    refactor(category meals): refactor category meals screen to use meal list component

--- screens/CategoryMealsScreen.tsx
+++ screens/CategoryMealsScreen.tsx
@@ -1,46 +1,16 @@
 import React from 'react'
 import { NavigationParams, NavigationScreenProp, NavigationState } from 'react-navigation'
-import { View, Text, StyleSheet, Button, FlatList } from 'react-native'
 import { CATEGORIES, MEALS } from '../data/dummy-data'
-import Meal from '../models/Meal'
-import MealItem from '../components/MealItem'
+import MealList from '../components/MealList'
 
 interface CategoryMealsScreenT {
   navigation: NavigationScreenProp<NavigationState, NavigationParams>
 }
 const CategoryMealsScreen = (props: CategoryMealsScreenT) => {
-  const renderMealItem = (itemData: { item: Meal }) => {
-    return (
-      <MealItem
-        title={itemData.item.title}
-        onSelect={() => {
-          props.navigation.navigate({
-            routeName: 'MealDetail',
-            params: {
-              mealId: itemData.item.id,
-            },
-          })
-        }}
-        duration={itemData.item.duration}
-        affordability={itemData.item.affordability}
-        complexity={itemData.item.complexity}
-        image={itemData.item.imageUrl}
-      />
-    )
-  }
   const catId = props.navigation.getParam('categoryId')
   const displayedMeals = MEALS.filter((meal) => meal.categoryIds.indexOf(catId) >= 0)
 
-  return (
-    <View style={styles.screen}>
-      <FlatList
-        data={displayedMeals}
-        renderItem={renderMealItem}
-        keyExtractor={(item, index) => item.id}
-        style={{ width: '100%' }}
-      />
-    </View>
-  )
+  return <MealList listData={displayedMeals} navigation={props.navigation} />
 }
 
 CategoryMealsScreen.navigationOptions = (navigationData: CategoryMealsScreenT) => {
@@ -50,13 +20,5 @@ CategoryMealsScreen.navigationOptions = (navigationData: CategoryMealsScreenT) =
     headerTitle: selectedCategory?.title,
   }
 }
-
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-})
 
 export default CategoryMealsScreen
